c98a0aa5c009ba8cdc33ce95dc79412d648ba377
ingyoe <melon7256@naver.com>
2022-03-24 20:43:34 +0900

    homework

--- 주차2/JavaHomework2/Number5_1.java
+++ 주차2/JavaHomework2/Number5_1.java
@@ -4,9 +4,7 @@ public class Number5_1 {
     public static void main(String[] args) {
         int count = 0;
         for (int i = 1; i <= 10000; i++) 
-        //8888
             for (int j = i; j > 0; j /= 10) 
-            //8888/10 = 888ㅈ
                 if (j % 10 == 8) 
                     count++;
         System.out.print(count);
---
+++ 주차2/JavaHomework2/tst.java
@@ -0,0 +1,25 @@
+package 주차2.JavaHomework2;
+
+public class tst {
+    public static void main(String[] args) {
+        String letter = "moom";
+    //     if(letter.charAt("noon".length()-2)==letter.charAt("noon".length()-"noon".length()+2)){
+            
+    //             System.out.println(1);
+            
+             
+    //     }
+    //  else{
+    //      System.out.println(-1);
+    //  }
+        System.out.println("noon".length());
+        System.out.println(letter.charAt(1));
+        if(letter.charAt(4-1)==letter.charAt(0)){
+            
+            
+            System.out.println(-1);
+        }else{
+            System.out.println(1);
+        }
+}
+}
