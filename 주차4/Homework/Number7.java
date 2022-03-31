3d9a224217e4c00728c32b81c174ea76d6165e8d
name <melon7256@naver.com>
2022-04-01 01:06:16 +0900

    homework

---
+++ 주차4/Homework/Number7.java
@@ -0,0 +1,10 @@
+public class Number7 {
+    public static void main(String[] args) {
+        int x[] = new int[100];
+        double y= 2;
+        for(int i =1; i<=100; i++){
+            y *= 2;
+        }
+        System.out.println(y);
+    }
+}
