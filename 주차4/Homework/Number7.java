7e625ff8f2f033245c60d68b229ee0d2431efea6
name <melon7256@naver.com>
2022-04-04 14:08:00 +0900

    javaPrac

--- 주차4/Homework/Number7.java
+++ 주차4/Homework/Number7.java
@@ -1,12 +1,14 @@
 package 주차4.Homework;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class Number7 {
     public static void main(String[] args) {
-        long x = (int)Math.pow(2, 100);
-        char i[] = Long.toString(x).toCharArray(); // 글자하나하나 때서 문자 배열로 저장
-        System.out.println(i.toString());
-        for(int j=0; j<i.length; j++){
-            System.out.print(i[j] - '0');
-        }
+        BigInteger k = BigInteger.valueOf(2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2*2);
+
+        String x = k.toString();
+        System.out.println(x);
+        System.out.println(x.getClass().getName());
     }
 }
